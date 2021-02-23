86478d275b93207de17541db1e409a9a4a1ac55e
Valaaz <val.zancoth@gmail.com>
2021-02-23 11:38:41 +0100

    added games rules

--- Projet-Java-S4-Client/src/mvc/controleur/ControleurMenuPrincipal.java
+++ Projet-Java-S4-Client/src/mvc/controleur/ControleurMenuPrincipal.java
@@ -7,8 +7,11 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Region;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -16,6 +19,12 @@ public class ControleurMenuPrincipal {
 
 	@FXML
 	private MenuItem menuItemQuitter;
+	@FXML
+	private MenuItem menuItemPendu;
+	@FXML
+	private MenuItem menuItemAllumettes;
+	@FXML
+	private MenuItem menuItemTicTacToe;
 	@FXML
 	private Button boutonPendu;
 
@@ -42,6 +51,54 @@ public class ControleurMenuPrincipal {
 	@FXML
 	public void quitterApplication() {
 		System.exit(0);
+	}
+
+	@FXML
+	public void reglesPendu() {
+		String regles = "Le pendu se joue avec l'ordinateur. Vous devez deviner le mot que l'ordinateur a choisi en sélectionnant une lettre."
+				+ " Un trait apparaît sur le dessin du pendu à chaque mauvaise réponse. Si vous trouver le mot vous gagnez, si le dessin arrive au bout de sa forme vous perdez.";
+
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Règles du Pendu");
+		alert.setHeaderText("Jeu du Pendu");
+		alert.setContentText(regles);
+		alert.setResizable(true);
+
+		// Permet à la fenêtre de s'adapter à la taille du contenu
+		alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+		alert.show();
+	}
+
+	@FXML
+	public void reglesAllumettes() {
+		String regles = "Ce jeu se joue avec l'ordinateur en tour par tour. Un nombre impair d'allumettes est disposé. Chacun des joueurs prend une ou 2 allumettes quand vient son tour."
+				+ " Le jeu s'arrête quand le tas est vide et le gagant est celui qui possède un nombre impair d'allumettes.";
+
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Règles du jeu des Allumettes");
+		alert.setHeaderText("Jeu des Allumettes");
+		alert.setContentText(regles);
+		alert.setResizable(true);
+
+		// Permet à la fenêtre de s'adapter à la taille du contenu
+		alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+		alert.show();
+	}
+
+	@FXML
+	public void reglesTicTacToe() {
+		String regles = "Le Tic-Tac-Toe ou Morpion se joue à 2 joueurs en tour par tour sur une grille en 3x3. Le gagnant est celui qui arrive aligner 3 de ses formes"
+				+ " que ce soit horizontalement, verticalement ou en diagonal. L'un des joueurs joue avec des ronds et l'autre avec des croix.";
+
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Règles du Tic-Tac-Toe");
+		alert.setHeaderText("Tic-Tac-Toe");
+		alert.setContentText(regles);
+		alert.setResizable(true);
+
+		// Permet à la fenêtre de s'adapter à la taille du contenu
+		alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+		alert.show();
 	}
 
 }
