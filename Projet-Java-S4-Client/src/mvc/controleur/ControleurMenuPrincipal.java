dcbe7625754ba00968f9bd5e45d10438bd4c1aac
Valaaz <val.zancoth@gmail.com>
2021-02-25 13:41:54 +0100

    initialize connection with the server

--- Projet-Java-S4-Client/src/mvc/controleur/ControleurMenuPrincipal.java
+++ Projet-Java-S4-Client/src/mvc/controleur/ControleurMenuPrincipal.java
@@ -1,19 +1,14 @@
 package mvc.controleur;
 
-import java.io.IOException;
-import java.net.URL;
+import java.rmi.Naming;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.Region;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+import mvc.modele.JeuInterface;
 
 public class ControleurMenuPrincipal {
 
@@ -31,21 +26,24 @@ public class ControleurMenuPrincipal {
 	@FXML
 	public void jeuDuPendu() {
 		try {
-			URL fxmlURL = getClass().getResource("/mvc/vue/FichePendu.fxml");
-			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
-			Parent root = fxmlLoader.load();
-
-			Stage stage = new Stage();
-
-			stage.setResizable(false);
-			stage.initModality(Modality.WINDOW_MODAL);
-			stage.setTitle("Pendu");
-			stage.setScene(new Scene(root, 500, 400));
-			stage.showAndWait(); // Permet, avec le code suivant, de rafraichir la table de donnees
-
-		} catch (IOException e) {
-			e.printStackTrace();
+			int port = 8000;
+			JeuInterface obj = (JeuInterface) Naming.lookup("rmi://localhost:" + port + "/hello");
+			System.out.println(obj.echo());
+			/*
+			 * URL fxmlURL = getClass().getResource("/mvc/vue/FichePendu.fxml"); FXMLLoader
+			 * fxmlLoader = new FXMLLoader(fxmlURL); Parent root = fxmlLoader.load();
+			 * 
+			 * Stage stage = new Stage();
+			 * 
+			 * stage.setResizable(false); stage.initModality(Modality.WINDOW_MODAL);
+			 * stage.setTitle("Pendu"); stage.setScene(new Scene(root, 500, 400));
+			 * stage.showAndWait(); // Permet, avec le code suivant, de rafraichir la table
+			 * de donnees
+			 */
+		} catch (Exception e) {
+			System.out.println("Client exception: " + e);
 		}
+
 	}
 
 	@FXML
