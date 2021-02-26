1ecfa2401f7930baa21e5677f01593db2a495bb1
Valaaz <val.zancoth@gmail.com>
2021-02-26 16:11:12 +0100

    draw beautiful grid

--- Projet-Java-S4-Client/src/mvc/controleur/ControleurJeuTicTacToe.java
+++ Projet-Java-S4-Client/src/mvc/controleur/ControleurJeuTicTacToe.java
@@ -1,5 +1,24 @@
 package mvc.controleur;
 
-public class ControleurJeuTicTacToe {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class ControleurJeuTicTacToe implements Initializable {
+
+	@FXML
+	private Label label00, label10, label20, label01, label11, label21, label02, label12, label22;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		label10.setStyle("-fx-border-color: black; -fx-border-style: hidden solid hidden solid;");
+		label11.setStyle("-fx-border-color: black;");
+		label12.setStyle("-fx-border-color: black; -fx-border-style: hidden solid hidden solid;");
+		label01.setStyle("-fx-border-color: black; -fx-border-style: solid hidden solid hidden;");
+		label21.setStyle("-fx-border-color: black; -fx-border-style: solid hidden solid hidden;");
+	}
 
 }
