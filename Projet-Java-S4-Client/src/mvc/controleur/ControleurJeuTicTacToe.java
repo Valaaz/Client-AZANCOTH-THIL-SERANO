aff7846d6224dee5060c0789351950354a4e208b
Valaaz <val.zancoth@gmail.com>
2021-02-26 18:05:57 +0100

    improved code and added actuel player

--- Projet-Java-S4-Client/src/mvc/controleur/ControleurJeuTicTacToe.java
+++ Projet-Java-S4-Client/src/mvc/controleur/ControleurJeuTicTacToe.java
@@ -12,10 +12,13 @@ import javafx.scene.input.MouseEvent;
 public class ControleurJeuTicTacToe implements Initializable {
 
 	@FXML
-	private Label label00, label10, label20, label01, label11, label21, label02, label12, label22;
+	private Label label00, label10, label20, label01, label11, label21, label02, label12, label22, tourJoueur;
+
+	public int tour = 1;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		tourJoueur.setText("Au tour du joueur " + tour);
 		label00.setOnMouseClicked(handler);
 		label10.setOnMouseClicked(handler);
 		label20.setOnMouseClicked(handler);
@@ -38,43 +41,57 @@ public class ControleurJeuTicTacToe implements Initializable {
 		public void handle(MouseEvent e) {
 			if (e.getSource() == label00) {
 				System.out.println(label00.getId());
-				label00.setText("X");
+				poseForme(label00);
 			}
 			if (e.getSource() == label10) {
 				System.out.println(label10.getId());
-				label10.setText("X");
+				poseForme(label10);
 			}
 			if (e.getSource() == label20) {
 				System.out.println(label20.getId());
-				label20.setText("X");
+				poseForme(label20);
 			}
 			if (e.getSource() == label01) {
 				System.out.println(label01.getId());
-				label01.setText("X");
+				poseForme(label01);
 			}
 			if (e.getSource() == label11) {
 				System.out.println(label11.getId());
-				label11.setText("X");
+				poseForme(label11);
 			}
 			if (e.getSource() == label12) {
 				System.out.println(label12.getId());
-				label12.setText("X");
+				poseForme(label12);
 			}
 			if (e.getSource() == label02) {
 				System.out.println(label02.getId());
-				label02.setText("X");
+				poseForme(label02);
 			}
 			if (e.getSource() == label21) {
 				System.out.println(label21.getId());
-				label21.setText("X");
+				poseForme(label21);
 			}
 			if (e.getSource() == label22) {
 				System.out.println(label22.getId());
-				label22.setText("X");
+				poseForme(label22);
 			}
 			e.consume();
 		}
 
 	};
+
+	public void poseForme(Label l) {
+		String joueurActuel = "Au tour du joueur ";
+		if (tour == 1) {
+			l.setText("X");
+			tour += 1;
+			joueurActuel += tour;
+		} else {
+			l.setText("O");
+			tour -= 1;
+			joueurActuel += tour;
+		}
+		tourJoueur.setText(joueurActuel);
+	}
 
 }
