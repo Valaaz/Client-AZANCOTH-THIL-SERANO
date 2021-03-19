7a3d238fc2bc3a828fec3c7f76bb1d1aae00c7ab
Valaaz <val.zancoth@gmail.com>
2021-03-19 18:06:49 +0100

    improved player waiting

--- Projet-Java-S4-Client/src/mvc/controleur/ControleurJeuTicTacToe.java
+++ Projet-Java-S4-Client/src/mvc/controleur/ControleurJeuTicTacToe.java
@@ -9,6 +9,7 @@ import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
 import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -31,6 +32,7 @@ public class ControleurJeuTicTacToe implements Initializable {
 	private static InterfaceTicTacToe intTtt;
 	private int numJoueur;
 	String[] labels;
+	private boolean partieFinie = false;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -71,30 +73,24 @@ public class ControleurJeuTicTacToe implements Initializable {
 			if (intTtt.getNombreJoueur(idPartie) == 1) {
 				tourJoueur.setText("En attente d'un deuxième joueur..");
 				numJoueur = 1;
-				new Thread(new Runnable() {
-					@Override
-					public void run() {
-						Platform.runLater(new Runnable() {
-							@Override
-							public void run() {
-								try {
-									while (intTtt.getNombreJoueur(idPartie) == 1) {
-										System.out.println("en attente..");
-										TimeUnit.SECONDS.sleep(2);
-									}
-									labelJoueur.setText("Vous êtes le joueur " + numJoueur);
-									System.out.println("JOUEUR " + numJoueur);
-
-									tourJoueur.setText("Au tour du joueur " + intTtt.getTourActuel(idPartie));
-
-									intTtt.setTourActuel(idPartie, tour);
-								} catch (RemoteException | InterruptedException e) {
-									System.out.println(e);
-								}
-							}
-						});
-					}
-				}).start();
+				/*
+				 * new Thread(new Runnable() {
+				 *
+				 * @Override public void run() { Platform.runLater(new Runnable() {
+				 *
+				 * @Override public void run() { try { while (intTtt.getNombreJoueur(idPartie)
+				 * == 1) { System.out.println("en attente.."); TimeUnit.SECONDS.sleep(2); }
+				 * labelJoueur.setText("Vous êtes le joueur " + numJoueur);
+				 * System.out.println("JOUEUR " + numJoueur);
+				 *
+				 * tourJoueur.setText("Au tour du joueur " + intTtt.getTourActuel(idPartie));
+				 *
+				 * intTtt.setTourActuel(idPartie, tour);
+				 *
+				 * } catch (RemoteException | InterruptedException e) { System.out.println(e); }
+				 * } }); } }).start();
+				 */
+				new Thread(task).start();
 
 			} else {
 				numJoueur = 2;
@@ -111,58 +107,116 @@ public class ControleurJeuTicTacToe implements Initializable {
 		}
 
 		// new Thread(task).start();
-
 	}
 
-	/*
-	 * Task<Void> task = new Task<Void>() {
-	 *
-	 * @Override public Void call() { try { if (intTtt.getNombreJoueur(idPartie) ==
-	 * 1) { tourJoueur.setText("En attente d'un deuxième joueur.."); numJoueur = 1;
-	 * while (intTtt.getNombreJoueur(idPartie) == 1) {
-	 * System.out.println("en attente.."); TimeUnit.SECONDS.sleep(2); } } else {
-	 * numJoueur = 2; } labelJoueur.setText("Vous êtes le joueur " + numJoueur);
-	 * System.out.println("JOUEUR " + numJoueur);
-	 *
-	 * tourJoueur.setText("Au tour du joueur " + intTtt.getTourActuel(idPartie));
-	 *
-	 * intTtt.setTourActuel(idPartie, tour); } catch (RemoteException |
-	 * InterruptedException e) { System.out.println(e); } return null; } };
-	 */
+	Task<Void> task = new Task<Void>() {
+		@Override
+		public Void call() {
+			try {
+				while (intTtt.getNombreJoueur(idPartie) == 1) {
+					System.out.println("en attente..");
+					TimeUnit.SECONDS.sleep(2);
+				}
 
-	/*
-	 * public void tour() { try { labels = intTtt.getLabels(idPartie);
-	 *
-	 * label1.setText(labels[0]); label2.setText(labels[1]);
-	 * label3.setText(labels[2]); label4.setText(labels[3]);
-	 * label5.setText(labels[4]); label6.setText(labels[5]);
-	 * label7.setText(labels[6]); label8.setText(labels[7]);
-	 * label9.setText(labels[8]);
-	 *
-	 * System.out.println("Tour : " + intTtt.getTourActuel(idPartie) +
-	 * ", Num joueur : " + numJoueur);
-	 *
-	 * if (intTtt.getTourActuel(idPartie) == 1 && numJoueur == 1 ||
-	 * intTtt.getTourActuel(idPartie) == 2 && numJoueur == 2) {
-	 * label1.setDisable(false); label2.setDisable(false); label3.setDisable(false);
-	 * label4.setDisable(false); label5.setDisable(false); label6.setDisable(false);
-	 * label7.setDisable(false); label8.setDisable(false); label9.setDisable(false);
-	 * } else { label1.setDisable(true); label2.setDisable(true);
-	 * label3.setDisable(true); label4.setDisable(true); label5.setDisable(true);
-	 * label6.setDisable(true); label7.setDisable(true); label8.setDisable(true);
-	 * label9.setDisable(true); }
-	 *
-	 * } catch (RemoteException e) { System.out.println("Tour exception " + e); } }
-	 */
+				Platform.runLater(new Runnable() {
 
-	/*
-	 * public void poseForme(Label l) { try { String joueurActuel =
-	 * "Au tour du joueur "; if (intTtt.getTourActuel(idPartie) == 1) {
-	 * l.setText("X"); intTtt.setTourActuel(idPartie, tour + 1); } else {
-	 * l.setText("O"); intTtt.setTourActuel(idPartie, tour - 1); } joueurActuel +=
-	 * intTtt.getTourActuel(idPartie); tourJoueur.setText(joueurActuel); } catch
-	 * (RemoteException e) { System.out.println("Tour exception " + e); } }
-	 */
+					@Override
+					public void run() {
+						labelJoueur.setText("Vous êtes le joueur " + numJoueur);
+						System.out.println("JOUEUR " + numJoueur);
+
+						try {
+							tourJoueur.setText("Au tour du joueur " + intTtt.getTourActuel(idPartie));
+							intTtt.setTourActuel(idPartie, tour);
+						} catch (RemoteException e) {
+							e.printStackTrace();
+						}
+					}
+				});
+
+			} catch (RemoteException | InterruptedException e) {
+				System.out.println(e);
+			}
+			return null;
+		}
+	};
+
+	Task<Void> joue = new Task<Void>() {
+
+		@Override
+		public Void call() {
+			while (partieFinie != true) {
+				tour();
+				try {
+					TimeUnit.SECONDS.sleep(5);
+				} catch (InterruptedException e) {
+					System.out.println("Time exception : " + e);
+				}
+			}
+			return null;
+		}
+	};
+
+	public void tour() {
+		try {
+			labels = intTtt.getLabels(idPartie);
+
+			label1.setText(labels[0]);
+			label2.setText(labels[1]);
+			label3.setText(labels[2]);
+			label4.setText(labels[3]);
+			label5.setText(labels[4]);
+			label6.setText(labels[5]);
+			label7.setText(labels[6]);
+			label8.setText(labels[7]);
+			label9.setText(labels[8]);
+
+			System.out.println("Tour : " + intTtt.getTourActuel(idPartie) + ", Num joueur : " + numJoueur);
+
+			if (intTtt.getTourActuel(idPartie) == 1 && numJoueur == 1
+					|| intTtt.getTourActuel(idPartie) == 2 && numJoueur == 2) {
+				label1.setDisable(false);
+				label2.setDisable(false);
+				label3.setDisable(false);
+				label4.setDisable(false);
+				label5.setDisable(false);
+				label6.setDisable(false);
+				label7.setDisable(false);
+				label8.setDisable(false);
+				label9.setDisable(false);
+			} else {
+				label1.setDisable(true);
+				label2.setDisable(true);
+				label3.setDisable(true);
+				label4.setDisable(true);
+				label5.setDisable(true);
+				label6.setDisable(true);
+				label7.setDisable(true);
+				label8.setDisable(true);
+				label9.setDisable(true);
+			}
+
+		} catch (RemoteException e) {
+			System.out.println("Tour exception " + e);
+		}
+	}
+
+	public void poseForme(Label l) {
+		try {
+			String joueurActuel = "Au tour du joueur ";
+			if (intTtt.getTourActuel(idPartie) == 1) {
+				l.setText("X");
+				intTtt.setTourActuel(idPartie, tour + 1);
+			} else {
+				l.setText("O");
+				intTtt.setTourActuel(idPartie, tour - 1);
+			}
+			joueurActuel += intTtt.getTourActuel(idPartie);
+			tourJoueur.setText(joueurActuel);
+		} catch (RemoteException e) {
+			System.out.println("Tour exception " + e);
+		}
+	}
 
 	public String getForme(int tour) {
 		tour--; // On décremente le tour de 1 car on l'incrémente dans la fonction poseForme()
@@ -178,56 +232,60 @@ public class ControleurJeuTicTacToe implements Initializable {
 		public void handle(MouseEvent e) {
 			try {
 				if (e.getSource() == label1 && label1.getText() == "") {
-					// poseForme(label1);
-					// labels[0] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label1);
+					labels[0] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				if (e.getSource() == label2 && label2.getText() == "") {
-					// poseForme(label2);
-					// labels[1] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label2);
+					labels[1] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				if (e.getSource() == label3 && label3.getText() == "") {
-					// poseForme(label3);
-					// labels[2] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label3);
+					labels[2] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				if (e.getSource() == label4 && label4.getText() == "") {
-					// poseForme(label4);
-					// labels[3] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label4);
+					labels[3] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				if (e.getSource() == label5 && label5.getText() == "") {
-					// poseForme(label5);
-					// labels[4] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label5);
+					labels[4] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				if (e.getSource() == label6 && label6.getText() == "") {
-					// poseForme(label6);
-					// labels[5] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label6);
+					labels[5] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				if (e.getSource() == label7 && label7.getText() == "") {
-					// poseForme(label7);
-					// labels[6] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label7);
+					labels[6] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				if (e.getSource() == label8 && label8.getText() == "") {
-					// poseForme(label8);
-					// labels[7] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label8);
+					labels[7] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				if (e.getSource() == label9 && label9.getText() == "") {
-					// poseForme(label9);
-					// labels[8] = getForme(intTtt.getTourActuel(idPartie));
+					poseForme(label9);
+					labels[8] = getForme(intTtt.getTourActuel(idPartie));
 				}
 				e.consume();
 
-				// intTtt.setLabels(idPartie, labels);
+				intTtt.setLabels(idPartie, labels);
 
 				if (intTtt.verificationVictoire(idPartie, tour, label1.getText(), label2.getText(), label3.getText(),
 						label4.getText(), label5.getText(), label6.getText(), label7.getText(), label8.getText(),
 						label9.getText()) == true) {
+					partieFinie = true;
 					// reinitialisation();
 				}
 				if (intTtt.verificationMatchNul(idPartie, label1.getText(), label2.getText(), label3.getText(),
 						label4.getText(), label5.getText(), label6.getText(), label7.getText(), label8.getText(),
 						label9.getText()) == true) {
+					partieFinie = true;
 					// reinitialisation();
 				}
-				// tour();
+
+				tour();
+
 			} catch (RemoteException re) {
 				System.out.println(re);
 			}
